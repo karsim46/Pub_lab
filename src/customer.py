722e6ccaf4380e291ba99793c4889f2c7c76ee01
Simon <simonwatson176@gmail.com>
2021-06-23 15:47:27 +0100

    last commit

--- src/customer.py
+++ src/customer.py
@@ -1,8 +1,13 @@
 class Customer:
-    def __init__(self, name, wallet, age):
+    def __init__(self, name, wallet, age, drunkeness_level):
         self.name = name
         self.wallet = wallet
         self.age = age
+        self.drunkeness_level = drunkeness_level
 
     def pay_drink(self, drink):
         self.wallet -= drink.price
+
+    def is_customer_pissed(self, drink):
+        drunkeness_level = 0
+
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -9,7 +9,7 @@ class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100)
         self.customer = Customer("Stephen", 100, 37)
-        self.drink = Drink("cider", 5)
+        self.drink = Drink("cider", 5, 5)
         
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self. pub.name)
@@ -21,4 +21,5 @@ class TestPub(unittest.TestCase):
     
     def test_age_check(self):
         self.assertEqual(True, self.pub.age_check(self.customer))
+    
         
